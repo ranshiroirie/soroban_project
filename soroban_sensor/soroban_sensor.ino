cdb10cc47df00b4628781070630f970b7b52543e
ranshiroirie <ranshiroirie@gmail.com>
2019-05-20 20:06:35 +0900

    アナログピン増加対応に伴い、マルチプレクサの処理プログラムを追加 in 2019/05/20

--- soroban_sensor/soroban_sensor.ino
+++ soroban_sensor/soroban_sensor.ino
@@ -1,11 +1,37 @@
 #define Threshold 600
 #define LEDPIN_COUNT 7
 
-const int PIN[5] = {14, 15, 16, 17, 18};
+//MUX 設定
+const int controlPin[4] = {3, 4, 5, 6};
+const int SIG_pin = 14;
+const int muxChannel[16][4] = {
+  {0, 0, 0, 0}, //channel 0
+  {1, 0, 0, 0}, //channel 1
+  {0, 1, 0, 0}, //channel 2
+  {1, 1, 0, 0}, //channel 3
+  {0, 0, 1, 0}, //channel 4
+  {1, 0, 1, 0}, //channel 5
+  {0, 1, 1, 0}, //channel 6
+  {1, 1, 1, 0}, //channel 7
+  {0, 0, 0, 1}, //channel 8
+  {1, 0, 0, 1}, //channel 9
+  {0, 1, 0, 1}, //channel 10
+  {1, 1, 0, 1}, //channel 11
+  {0, 0, 1, 1}, //channel 12
+  {1, 0, 1, 1}, //channel 13
+  {0, 1, 1, 1}, //channel 14
+  {1, 1, 1, 1} //channel 15
+};
+
+const int PIN[5] = {0, 1, 2, 3, 4};
 const int LEDPIN[LEDPIN_COUNT] = {7, 8, 9, 10, 11, 12, 13};
 
 void setup() {
   Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    pinMode(controlPin[i], OUTPUT);
+    digitalWrite(controlPin[i], LOW);
+  }
   for (int i = 0; i < LEDPIN_COUNT; i++) {
     pinMode(LEDPIN[i], OUTPUT);
   }
@@ -15,7 +41,7 @@ void loop() {
   int juzu[5];
   int pinread [5];
   for (int i = 0; i < 5; i++) {
-    pinread[i] = analogRead(PIN[i]);
+    pinread[i] = readMux(PIN[i]);
     //    Serial.print(pinread[i]);
     //    Serial.print(" ");
     if (pinread[i] >= Threshold) {
@@ -69,4 +95,15 @@ void loop() {
   Serial.println(count);
   Serial.write(count);
   delay(100);
+}
+
+int readMux(int channel) {
+  //loop through the 4 sig
+  for (int i = 0; i < 4; i ++) {
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
+  //read the value at the SIG pin
+  int val = analogRead(SIG_pin);
+  //return the value
+  return val;
 }
