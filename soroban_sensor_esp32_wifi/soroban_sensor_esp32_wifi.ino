4be534d6eda68c8b64b329d575db6b2bdeb8edd8
ranshiroirie <ranshiroirie@gmail.com>
2019-06-12 13:11:57 +0900

    HTTPクライアント(POST)による送信プログラムの追記

---
+++ soroban_sensor_esp32_wifi/soroban_sensor_esp32_wifi.ino
@@ -0,0 +1,152 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+
+#define Threshold 550 //センサーの域値
+#define LEDPIN_COUNT 7 //出力するLEDのピン数
+#define SENSOR 15 //フォトリフレクタの数
+
+//Client設定
+#define ssid      "aqwlan-iot-int"
+#define password  "AsiaIoT0138"
+#define host  "https://035x3t4ci0.execute-api.ap-northeast-1.amazonaws.com/Prod/debug"
+const char* root_ca = \
+                      "-----BEGIN CERTIFICATE-----\n" \
+     "MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n" \
+     "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n" \
+     "DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n" \
+     "SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n" \
+     "GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n" \
+     "AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n" \
+     "q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n" \
+     "SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n" \
+     "Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n" \
+     "a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n" \
+     "/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n" \
+     "AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n" \
+     "CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n" \
+     "bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n" \
+     "c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n" \
+     "VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n" \
+     "ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n" \
+     "MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n" \
+     "Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n" \
+     "AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n" \
+     "uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n" \
+     "wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n" \
+     "X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n" \
+     "PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n" \
+     "KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n" \
+     "-----END CERTIFICATE-----\n";
+WiFiClientSecure client;
+
+//MUX 設定
+const int controlPin[4] = {16, 17, 18, 19};
+const int SIG_pin = 33;
+const int muxChannel[16][4] = {  //MUXのチャネル
+  {0, 0, 0, 0}, //channel 0
+  {1, 0, 0, 0}, //channel 1
+  {0, 1, 0, 0}, //channel 2
+  {1, 1, 0, 0}, //channel 3
+  {0, 0, 1, 0}, //channel 4
+  {1, 0, 1, 0}, //channel 5
+  {0, 1, 1, 0}, //channel 6
+  {1, 1, 1, 0}, //channel 7
+  {0, 0, 0, 1}, //channel 8
+  {1, 0, 0, 1}, //channel 9
+  {0, 1, 0, 1}, //channel 10
+  {1, 1, 0, 1}, //channel 11
+  {0, 0, 1, 1}, //channel 12
+  {1, 0, 1, 1}, //channel 13
+  {0, 1, 1, 1}, //channel 14
+  {1, 1, 1, 1} //channel 15
+};
+
+void setup() {
+  Serial.begin(115200);
+  for (int i = 0; i < 4; i++) {
+    pinMode(controlPin[i], OUTPUT);
+    digitalWrite(controlPin[i], LOW);
+  }
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setCACert(root_ca);
+
+  Serial.println("\nStarting connection to server...");
+  if (!client.connect(host, 443))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+  }
+}
+
+void loop() {
+  int juzu[SENSOR];
+  int pinread [SENSOR];
+  for (int i = 0; i < SENSOR; i++) {
+    pinread[i] = map(readMux(i), 0, 5000, 0, 1023);
+    //    Serial.print(pinread[i]);
+    //    Serial.print(" ");
+    if (pinread[i] >= Threshold) { //センサーに珠があるかどうか
+      juzu[i] = 1; //ある
+    } else {
+      juzu[i] = 0; //ない
+    }
+  }
+  //  Serial.println("");
+
+  //数字に変換するプログラム
+  int count[SENSOR / 5]; //数字の桁ごとの配列
+  int amount = 0; //合計
+  for (int i = 0; i < SENSOR / 5; i++) {
+    if (juzu[1 + i * 5] == 0) { //0
+      count[i] = 0;
+    } else if (juzu[2 + i * 5] == 0) { //1
+      count[i] = 1;
+    } else if (juzu[3 + i * 5] == 0) { //2
+      count[i] = 2;
+    } else if (juzu[4 + i * 5] == 0) { //3
+      count[i] = 3;
+    } else { //4
+      count[i] = 4;
+    }
+    if (juzu[0 + i * 5] == 1) { //5を足すかどうか
+      count[i] += 5;
+    }
+  }
+  for (int i = 2; i >= 0; i--) {
+    Serial.print(count[i]);
+  }
+  Serial.println();
+  //    amount = count[0] + count[1] * 10 + count[2] * 100;
+  //    Serial.println(amount);
+  delay(100);
+}
+
+int readMux(int channel) {
+  //loop through the 4 sig
+  for (int i = 0; i < 4; i ++) {
+    digitalWrite(controlPin[i], muxChannel[channel][i]);
+  }
+  //read the value at the SIG pin
+  int val = analogRead(SIG_pin);
+  //return the value
+  return val;
+}
+
+void post() {
+
+}
